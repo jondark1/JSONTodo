169af51e2581c7fe31e67240cf51dd2ab480644e
jondark1 <allivesjonneL@gmail.com>
2025-02-21 16:41:21 +0800

    JSON repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 import {
-  Route, createBrowerRouter,
+  Route,
+  createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider
-}
-  from "react-router-dom"
-import MainLayout from '@/layout/MainLayout'
-import Todos from './pages/Todos'
-import Todo from './pages/Todo'
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "@/layouts/MainLayout";
+import Todos from "./pages/Todos";
+import Todo from "./pages/Todo";
+import AddTodo from "./pages/AddTodo";
 
-const router = createBrowerRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />} >
-      <Route index element={<Todos />} />
-      <Route path="/todo/:id" element={<Todo />} />
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Todos/>} />
+      <Route path="/todo/:id" element={<Todo/>} />
+      <Route path="/add-todo" element={<AddTodo/>} />
     </Route>
   )
-)
+);
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
+};
 
-}
-
-export default App
+export default App;
